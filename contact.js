2942d17d93aed0caf20cf745eb57ca797e316259
Tom1358 <thomasellis358@hotmail.com>
2021-06-27 19:08:46 +0000

    Complete function in contact.js for email functionality

--- contact.js
+++ contact.js
@@ -1,7 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-
-
-
-
+function sendMail(contactForm) {
+    emailjs.send('gmail', 'template_1e3ugzs', {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "information_request": contactForm.projectsummary.value,
+    })
+    .then(
+        function(response) {
+            console.log('Success!', response);
+        }, 
+        function(error) {
+            console.log('Failed', error);
+        }
+    );
+    return false;
 }
