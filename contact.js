01d2dda5a0caa48b9c24753698ac9345ee2fbf17
Tom1358 <thomasellis358@hotmail.com>
2021-06-30 20:23:00 +0000

    Amend contact form function so that it correctly returns all desired values in an email to my email box

--- contact.js
+++ contact.js
@@ -1,18 +1,23 @@
-function sendMail(contactForm) {
-    emailjs.send('gmail', 'template_1e3ugzs', {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "information_request": contactForm.information.value,
-    })
-    .then(
-        function(response) {
-            console.log('Success!', response);
-        }, 
-        function(error) {
-            console.log('Failed', error);
-        }
-    );
-    return false;
-}
+window.onload = function() {
 
-console.log('working')
+    
+
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        form_submit.value = 'Sending...';
+
+        let serviceID = 'default_service';
+        let templateID = 'milestone_two';
+
+        emailjs.sendForm(serviceID, templateID, document.querySelector('#contact-form'))
+            .then(() => {
+                form_submit.value = 'Send Email';
+                alert('Sent!');
+            }, (err) => {
+                form_submit.value = 'Send Email';
+                alert(JSON.stringify(err));
+            }); return false;
+});
+
+}
