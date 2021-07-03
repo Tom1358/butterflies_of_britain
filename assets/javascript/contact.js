130b5d979e32e07befb2d947339b34c01883d2c8
Tom1358 <thomasellis358@hotmail.com>
2021-07-03 16:51:24 +0000

    Add notes to contact.js to help clarify what code is doing

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -1,23 +1,22 @@
 window.onload = function() {
-
     
-
+    //function for sending email
     document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
+        event.preventDefault(); // prevent immediately submitting to have 'sending' show in 'submit' button - source https://medium.com/weekly-webtips/emailjs-send-emails-directly-from-your-client-side-javascript-code-d85a6b23ff82
 
         form_submit.value = 'Sending...';
 
         let serviceID = 'default_service';
         let templateID = 'milestone_two';
 
-        emailjs.sendForm(serviceID, templateID, document.querySelector('#contact-form'))
+        emailjs.sendForm(serviceID, templateID, document.querySelector('#contact-form')) // uses above values to fill in necessary variables
             .then(() => {
                 form_submit.value = 'Send Email';
                 alert('Sent!');
             }, (err) => {
                 form_submit.value = 'Send Email';
-                alert(JSON.stringify(err));
-            }); return false;
+                alert(JSON.stringify(err)); // returns error to debug if necessary
+            }); return false; // prevents all information in form disappearing when sent - so user has reference to what they have sent
 });
 
 }
